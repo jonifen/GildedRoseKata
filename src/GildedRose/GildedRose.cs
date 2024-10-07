816ea517343ceef27db678df6600dc4c9a23a6c0
Jon Cain <jonifen@users.noreply.github.com>
2024-10-07 21:24:41 +0100

    Remove unnecessary nesting by consolidating conditionals

--- src/GildedRose/GildedRose.cs
+++ src/GildedRose/GildedRose.cs
@@ -14,42 +14,24 @@ namespace GildedRoseKata
         {
             for (var i = 0; i < Items.Count; i++)
             {
-                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert" && Items[i].Name != "Sulfuras, Hand of Ragnaros" && Items[i].Quality > 0)
                 {
-                    if (Items[i].Quality > 0)
-                    {
-                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            Items[i].Quality = Items[i].Quality - 1;
-                        }
-                    }
+                    Items[i].Quality = Items[i].Quality - 1;
                 }
 
-                if (Items[i].Name == "Aged Brie" || Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                if ((Items[i].Name == "Aged Brie" || Items[i].Name == "Backstage passes to a TAFKAL80ETC concert") && Items[i].Quality < 50)
                 {
-                    if (Items[i].Quality < 50)
-                    {
-                        Items[i].Quality = Items[i].Quality + 1;
+                    Items[i].Quality = Items[i].Quality + 1;
+                }
 
-                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].SellIn < 11)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
+                if ((Items[i].Name == "Aged Brie" || Items[i].Name == "Backstage passes to a TAFKAL80ETC concert") && Items[i].Quality < 50 && Items[i].Name == "Backstage passes to a TAFKAL80ETC concert" && Items[i].SellIn < 11 && Items[i].Quality < 50)
+                {
+                    Items[i].Quality = Items[i].Quality + 1;
+                }
 
-                            if (Items[i].SellIn < 6)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-                        }
-                    }
+                if ((Items[i].Name == "Aged Brie" || Items[i].Name == "Backstage passes to a TAFKAL80ETC concert") && Items[i].Quality < 50 && Items[i].Name == "Backstage passes to a TAFKAL80ETC concert" && Items[i].SellIn < 6 && Items[i].Quality < 50)
+                {
+                    Items[i].Quality = Items[i].Quality + 1;
                 }
 
                 if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
@@ -57,23 +39,21 @@ namespace GildedRoseKata
                     Items[i].SellIn = Items[i].SellIn - 1;
                 }
 
-                if (Items[i].SellIn < 0)
+                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert" && Items[i].Name != "Sulfuras, Hand of Ragnaros" && Items[i].Quality > 0 && Items[i].SellIn < 0)
                 {
-                    if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert" && Items[i].Name != "Sulfuras, Hand of Ragnaros" && Items[i].Quality > 0)
-                    {
-                        Items[i].Quality = Items[i].Quality - 1;
-                    }
-
-                    if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        Items[i].Quality = Items[i].Quality - Items[i].Quality;
-                    }
-
-                    if (Items[i].Name == "Aged Brie" && Items[i].Quality < 50)
-                    {
-                        Items[i].Quality = Items[i].Quality + 1;
-                    }
+                    Items[i].Quality = Items[i].Quality - 1;
                 }
+
+                if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert" && Items[i].SellIn < 0)
+                {
+                    Items[i].Quality = Items[i].Quality - Items[i].Quality;
+                }
+
+                if (Items[i].Name == "Aged Brie" && Items[i].Quality < 50 && Items[i].SellIn < 0)
+                {
+                    Items[i].Quality = Items[i].Quality + 1;
+                }
+
             }
         }
     }
