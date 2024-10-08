ac73cb1b5d2f6d5104fc0cd24a025dad723237b3
Jon Cain <jonifen@users.noreply.github.com>
2024-10-08 21:40:28 +0100

    Test the conjured item with quality of 1 will not result in negative quality

--- src/GildedRoseTests/UnitTests/ConjuredDegradesInQualityTwiceAsFast.cs
+++ src/GildedRoseTests/UnitTests/ConjuredDegradesInQualityTwiceAsFast.cs
@@ -15,4 +15,17 @@ public class ConjuredDegradesInQualityTwiceAsFast : WhenTestingGildedRose
 
     Assert.Equal(3, newItem.Quality);
   }
+
+  [Fact]
+  public void AConjuredItemWithQualityOf1_WillReturnQualityOf0()
+  {
+    // __"Conjured"__ items degrade in `Quality` twice as fast as normal items
+    // The `Quality` of an item is never negative
+    var newItem = AddItem("Conjured", 1, 5);
+    Setup();
+
+    GildedRose.UpdateQuality();
+
+    Assert.Equal(0, newItem.Quality);
+  }
 }
